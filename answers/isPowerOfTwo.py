6f3d8f5ee18217558f0166af1252d7b296012181
xiongxin <xiongxin@songxiaocai.com>
2021-05-30 09:56:21 +0800

    231

--- answers/isPowerOfTwo.py
+++ answers/isPowerOfTwo.py
@@ -2,9 +2,7 @@
 
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n == 0:
-            return False
-        return n & (n-1) == 0
+        return n > 0 and n & (n-1) == 0
 
 
 if __name__ == '__main__':
